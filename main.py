c4f8767f78ebe826245f9fe799d667b0a9cf7a2e
levi dominighini <35814710-sparrowhilllevi@users.noreply.replit.com>
2024-12-06 17:10:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#tic tac toe console-based
+#
+#this program will allow the player to play tic tac toe against 
+#the computer, the board will be drawn in text-characters on
+#the screen
+#
+#author: levi dominighini   
+#date: 2024-05-12
+
+def draw_board ():
+  print("   |   |   ")
+  print(" 1 | 2 | 3 ")
+  print("   |   |   ")
+  print("---+---+---")
+  print("   |   |   ")
+  print(" 4 | 5 | 6 ")
+  print("   |   |   ")
+  print("---+---+---")
+  print("   |   |   ")
+  print(" 7 | 8 | 9 ")
+  print("   |   |   ")
+
+draw_board()
