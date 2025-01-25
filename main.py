fc4a0a77dcb9991d5e9527b9c0d9d157266f33b1
musketman12 <sparrowhilllevi@gmail.com>
2025-01-25 16:12:04 +0000

    add a parameter to draw_board so we can pass the board model;  add a get_symbol function to retrieve the symbol for a specific square on the board

--- main.py
+++ main.py
@@ -14,18 +14,24 @@ board = [
    EMPTY, EMPTY, EMPTY
 ]
 
+def get_symbol(the_board, square_number):
+   symbol = the_board[square_number - 1]
+   if symbol == EMPTY:
+    return square_number
+
+   return symbol
+   
 def draw_board (the_board):
   print("   |   |   ")
-  print(" {} | {} | {} ".format(1,2,3))
+  print(" {} | {} | {} ".format (get_symbol(the_board, 1), get_symbol(the_board, 2), get_symbol(the_board, 3)))
   print("   |   |   ")
   print("---+---+---")
   print("   |   |   ")
-  print(" {} | {} | {} ".format(4,5,6))
+  print(" {} | {} | {} ".format(get_symbol(the_board, 4), get_symbol(the_board, 5), get_symbol(the_board, 6)))
   print("   |   |   ")
   print("---+---+---")
   print("   |   |   ")
-  print(" {} | {} | {} ".format(7,8,9))
+  print(" {} | {} | {}" .format(get_symbol(the_board, 7), get_symbol(the_board, 8), get_symbol(the_board, 9))) 
   print("   |   |   ")
 
 draw_board(board)
- 
